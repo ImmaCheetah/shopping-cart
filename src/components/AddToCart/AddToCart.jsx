7be642f8e1d48e5430d345e42323ee42c1b34f23
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-08-14 11:14:20 -0400

    feat: move cart to AddToCart and handle changes inside

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,7 +1,24 @@
 import { useState } from "react"
+import { useOutletContext } from "react-router-dom";
 
-export default function AddToCart({onClick, quantity}) {
+export default function AddToCart({title, price, imgUrl}) {
     const [productQty, setProductQty] = useState(1)
+    const {cart, setCart} = useOutletContext()
+    console.log(cart)
+
+    function handleClick() {
+        setCart(prevCart => {
+            return ([
+                ...prevCart, 
+                {
+                    title,
+                    price,
+                    imgUrl,
+                    productQty
+                }
+            ])
+        })
+    }
 
     function incrementQty() {
         if (productQty >= 99) {
@@ -35,7 +52,7 @@ export default function AddToCart({onClick, quantity}) {
             <button onClick={decrementQty}>-</button>
             <input type="number" value={productQty} onChange={handleChange}/>
             <button onClick={incrementQty}>+</button>
-            <button onClick={onClick}>Add to Cart</button>
+            <button onClick={handleClick}>Add to Cart</button>
         </div>
     )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,30 +5,19 @@ import styles from './Card.module.css'
 import AddToCart from '../AddToCart/AddToCart'
 
 export default function Card({title, price, imgUrl}) {
-    const {cart, setCart} = useOutletContext()
-    const [quantity, setQuantity] = useState(1);
-    console.log('CART', cart)
-
-    function handleClick() {
-        setCart(prevCart => {
-            return ([
-                ...prevCart, 
-                {
-                    title,
-                    price,
-                    imgUrl,
-                    quantity
-                }
-            ])
-        })
-    }
+    
+    
 
     return (
         <div>
             <img src={imgUrl} alt="" className={styles.productImage}/>
             <p>{title}</p>
             <p>{price}</p>
-            <AddToCart onClick={handleClick} quantity={quantity}/>
+            <AddToCart
+                title={title}
+                price={price}
+                imgUrl={imgUrl}
+            />
         </div>
     )
 }
