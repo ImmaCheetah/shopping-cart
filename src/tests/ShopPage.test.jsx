5037a3466351dc74bf9fc3922da8471d426d5563
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-08-15 11:57:13 -0400

    test: add test to check carta nd quantity changes

--- src/tests/ShopPage.test.jsx
+++ src/tests/ShopPage.test.jsx
@@ -1,8 +1,9 @@
-import { describe, it, expect, vi, afterEach } from 'vitest';
-import { render, screen, cleanup } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import routes from '../routes';
+import { act } from 'react';
 
 global.fetch = vi.fn(() => {
     const data = [{ id: '1', title: 'shirt'}]
@@ -12,27 +13,49 @@ global.fetch = vi.fn(() => {
     });
   })
 
-const router = createMemoryRouter(routes, {initialEntries: ['/shop']})
-
 describe('Quantity changes', () => {
-    render(<RouterProvider router={router} />)
-    const user = userEvent.setup()
-    
     it('adds 1 to quantity', async () => {
+        const router = createMemoryRouter(routes, {initialEntries: ['/shop']})
+        await act(() => {
+            render(<RouterProvider router={router} />)
+        })
+
+        const user = userEvent.setup()
         const plusBtn = screen.getByRole('button', {name: '+'})
         const input = screen.getByLabelText(/qty/i)
-        screen.debug(undefined, 10000)
+        screen.debug()
         
         await user.click(plusBtn)
         
         expect(input.value).toBe('2')
     })
+
+    it('does not go below 1', async () => {
+        const router = createMemoryRouter(routes, {initialEntries: ['/shop']})
+        await act(() => {
+            render(<RouterProvider router={router} />)
+        })
+
+        const user = userEvent.setup()
+        const minusBtn = screen.getByRole('button', {name: '-'})
+        const input = screen.getByLabelText(/qty/i)
+        screen.debug()
+        
+        await user.click(minusBtn)
+        
+        expect(input.value).toBe('1')
+    })
     
     it('changes cart number when item added', async () => {
+        const router = createMemoryRouter(routes, {initialEntries: ['/shop']})
+        await act(() => {
+            render(<RouterProvider router={router} />)
+        })
+        const user = userEvent.setup()
         const addBtn = screen.getByText('Add to Cart')
-        screen.debug(undefined, 10000)
         const cartLink = screen.getByRole('link', {name: /cart/i})
-        
+        screen.debug()
+
         await user.click(addBtn)
         
         expect(cartLink.textContent).toBe('Cart 1')
